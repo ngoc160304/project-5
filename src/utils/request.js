f63d889f61eacd233761b08d8235113cff85176c
ngoc160304 <ngocdev160304@gmail.com>
2023-11-01 01:34:38 +0700

    update request

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,13 +6,18 @@ export const get = async (path) => {
 }
 export const post = async (path, option) => {
     const response = await fetch(DOMAIN + path, {
-        method: "POST",
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
         headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded',
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: JSON.stringify(option)
-    })
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(option), // body data type must match "Content-Type" header
+    });
     const data =  await response.json();
     return data;
 }
