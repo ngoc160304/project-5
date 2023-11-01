1ea2da901f991993dfd4219be1d39bd12a6d0dff
ngoc160304 <ngocdev160304@gmail.com>
2023-11-01 10:24:58 +0700

    update request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,4 @@
-const DOMAIN = "https://data-project-5.vercel.app/"
+const DOMAIN = "https://jjryt6-8080.csb.app/"
 export const get = async (path) => {
     const response = await fetch(DOMAIN + path);
     const data =  await response.json();
@@ -6,17 +6,11 @@ export const get = async (path) => {
 }
 export const post = async (path, option) => {
     const response = await fetch(DOMAIN + path, {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
+        method: "POST", 
         headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
+            "Content-Type": "application/json",
         },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(option), // body data type must match "Content-Type" header
+        body: JSON.stringify(option), 
     });
     const data =  await response.json();
     return data;
